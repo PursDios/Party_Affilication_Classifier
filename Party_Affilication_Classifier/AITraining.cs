1a4c2ccfd55e4b7a14571939643e9a78ada56266
Ryan Cooper <pursdios942@gmail.com>
2018-10-20 00:29:56 +0100

    Added functionality to add the words to the categories and the frequency of the words.

--- Party_Affilication_Classifier/AITraining.cs
+++ Party_Affilication_Classifier/AITraining.cs
@@ -9,9 +9,14 @@ namespace Party_Affilication_Classifier
 {
     public class AITraining
     {
-        public Dictionary<string,int> Labour { get; set; }
-        public Dictionary<string,int> Conservative { get; set; }
-        public Dictionary<string,int> Coalition { get; set; }
+        private Dictionary<string, int> m_Labour = new Dictionary<string, int>();
+        public Dictionary<string,int> get_Labour { get { return m_Labour; } set { m_Labour = value; } }
+
+        private Dictionary<string, int> m_Conservative = new Dictionary<string, int>();
+        public Dictionary<string,int> get_Conservative { get { return m_Conservative; } set { m_Conservative = value; } }
+
+        private Dictionary<string, int> m_Coalition = new Dictionary<string, int>();
+        public Dictionary<string,int> Coalition { get { return m_Coalition; } set { m_Coalition = value; } }
 
         //The files selected for training being ordered in order of category.
         Dictionary<FileInfo, string> fileCats = new Dictionary<FileInfo, string>();
@@ -43,15 +48,57 @@ namespace Party_Affilication_Classifier
             {
                 if (f.Value == "Labour")
                 {
-                    sr = new StreamReader (f.Key.ToString());
+                    sr = new StreamReader (@"TrainingFiles\" +  f.Key.ToString());
+                    string s = sr.ReadToEnd();
+                    string[] splitWords = s.Split(' ');
+
+                    foreach(string str in splitWords)
+                    {
+                        if(!get_Labour.ContainsKey(str))
+                        {
+                            get_Labour.Add(str, 1);
+                        }
+                        else
+                        {
+                            get_Labour[str]++;
+                        }
+                    }
                 }
                 if (f.Value == "Conservative")
                 {
+                    sr = new StreamReader(@"TrainingFiles\" + f.Key.ToString());
+                    string s = sr.ReadToEnd();
+                    string[] splitWords = s.Split(' ');
 
+                    foreach (string str in splitWords)
+                    {
+                        if (!get_Labour.ContainsKey(str))
+                        {
+                            get_Labour.Add(str, 1);
+                        }
+                        else
+                        {
+                            get_Labour[str]++;
+                        }
+                    }
                 }
                 if (f.Value == "Coalition")
                 {
+                    sr = new StreamReader(@"TrainingFiles\" + f.Key.ToString());
+                    string s = sr.ReadToEnd();
+                    string[] splitWords = s.Split(' ');
 
+                    foreach (string str in splitWords)
+                    {
+                        if (!get_Labour.ContainsKey(str))
+                        {
+                            get_Labour.Add(str, 1);
+                        }
+                        else
+                        {
+                            get_Labour[str]++;
+                        }
+                    }
                 }
             }
         }
